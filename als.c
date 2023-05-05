112ce45aa4f80e18205dcfd3484ffacd5ae29ba9
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-05 12:27:18 +0500

    Проверки на невалидные аргументы

--- als.c
+++ als.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 
 int* lsearch(int* begin, int* end, int elem, comp comp) {
+  if (NULL == begin) {
+    return NULL;
+  }
+
+  if (NULL == end) {
+    return NULL;
+  }
+
+  if (begin >= end) {
+    return NULL;
+  }
+
+  if (NULL == comp) {
+    return NULL;
+  }
+
   while (begin != end) {
     int cmpres = comp(*begin, elem);
     if (cmpres == 0) {
