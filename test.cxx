d5b2f734bfe3018e11af6ade55c11431f395d93f
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-06 11:27:04 +0500

    Тесты строк

--- test.cxx
+++ test.cxx
@@ -6,7 +6,7 @@
 #include "eq.h"
 
 void
-test_integers() {
+test_integers(void) {
   int data[]{1, 2, 3};
 
   auto has{[begin = &data[0], end = &data[0] + 3](void* elem_to_search,
@@ -43,7 +43,56 @@ test_integers() {
   std::cout << "Integers: OK" << '\n';
 }
 
+void test_strings(void) {
+  constexpr size_t strsz{32u};
+
+  char data[][strsz]{
+    "123",
+    "456",
+  };
+
+  auto has{[begin = &data[0], end = &data[0] + 3](void* elem_to_search,
+                                                  size_t elem_size) {
+    void* found{lsearch(begin, end, elem_to_search, elem_size, eq)};
+    return end != found;
+  }};
+
+  {
+    char elem_to_search[strsz]{"0"};
+    assert(!has(elem_to_search, sizeof elem_to_search));
+  }
+
+  {
+    char elem_to_search[strsz]{"1"};
+    // "1" - это подстрока "123"
+    assert(has(elem_to_search, 1u));
+  }
+
+  {
+    char elem_to_search[strsz]{"123"};
+    assert(has(elem_to_search, sizeof elem_to_search));
+  }
+
+  {
+    char elem_to_search[strsz]{"456"};
+    assert(has(elem_to_search, sizeof elem_to_search));
+  }
+
+  {
+    char elem_to_search[strsz]{"123456"};
+    assert(!has(elem_to_search, sizeof elem_to_search));
+  }
+
+  {
+    char elem_to_search[strsz]{"789"};
+    assert(!has(elem_to_search, sizeof elem_to_search));
+  }
+
+  std::cout << "Strings: OK" << '\n';
+}
+
 int
 main(void) {
   test_integers();
+  test_strings();
 }
