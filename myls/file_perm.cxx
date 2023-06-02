c3dbec317abab9cf894474ccacba95ac30dc3d68
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-06-02 19:03:16 +0500

    Начата работа со считания прав доступа к файлу

---
+++ myls/file_perm.cxx
@@ -0,0 +1,35 @@
+#include "file_perm.hxx"
+#include <sys/stat.h>
+#include <iostream>
+
+myls::file_perm::file_perm(const std::string& name)
+    : mask{0b000'000'000u}, name{name} {
+
+  int error_status{::stat(name.c_str(), &stat)};
+
+  fill_owner_mask();
+  fill_group_mask();
+  fill_other_mask();
+}
+
+void myls::file_perm::fill_owner_mask(void) {
+  if (S_IRUSR == (S_IRUSR & stat.st_mode)) {
+    mask |= static_cast<std::uint16_t>(myls::permissions::owner_read);
+  }
+
+  if (S_IWUSR == (S_IWUSR & stat.st_mode)) {
+    mask |= static_cast<std::uint16_t>(myls::permissions::owner_write);
+  }
+
+  if (S_IXUSR == (S_IXUSR & stat.st_mode)) {
+    mask |= static_cast<std::uint16_t>(myls::permissions::owner_execute);
+  }
+}
+
+void myls::file_perm::fill_group_mask(void) {
+
+}
+
+void myls::file_perm::fill_other_mask(void) {
+
+}
---
+++ myls/file_perm.hxx
@@ -0,0 +1,43 @@
+#ifndef FILE_PERM_HXX
+#define FILE_PERM_HXX
+
+#include <cstdint>
+#include <string>
+#include <sys/stat.h>
+
+namespace myls {
+  class file_perm;
+  enum class permissions : std::uint16_t;
+}
+
+enum class myls::permissions : std::uint16_t {
+  owner_read = 0b100'000'000u,
+  owner_write = 0b010'000'000u,
+  owner_execute = 0b001'000'000u,
+
+  group_read = 0b000'100'000u,
+  group_write = 0b000'010'000u,
+  group_execute = 0b000'001'000u,
+
+  other_read = 0b000'000'100u,
+  other_write = 0b000'000'010u,
+  other_execute = 0b000'000'001u
+};
+
+
+class myls::file_perm {
+  std::uint16_t mask : 9u;
+  std::string name{};
+
+  using sys_stat = struct stat;
+  sys_stat stat{};
+
+  void fill_owner_mask(void);
+  void fill_group_mask(void);
+  void fill_other_mask(void);
+
+ public:
+  file_perm(const std::string& name);
+};
+
+#endif
