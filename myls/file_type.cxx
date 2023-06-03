b861072ea6798a740535d5663b22477d5dd0caff
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-06-03 12:41:00 +0500

    Класс для получения типа файла

---
+++ myls/file_type.cxx
@@ -0,0 +1,73 @@
+#include "file_type.hxx"
+#include <iostream>
+#include <sys/types.h>
+
+myls::file_type::file_type(const std::string& name) : type{0b0000000u} {
+  int error{::stat(name.c_str(), &stat)};
+  fill_type();
+};
+
+void myls::file_type::fill_type(void) {
+  if (0 != S_ISDIR(stat.st_mode)) {
+    type = myls::file_types::directory;
+    return;
+  }
+
+  if (0 != S_ISCHR(stat.st_mode)) {
+    type = myls::file_types::character;
+    return;
+  }
+
+  if (0 != S_ISBLK(stat.st_mode)) {
+    type = myls::file_types::block;
+    return;
+  }
+
+  if (0 != S_ISREG(stat.st_mode)) {
+    type = myls::file_types::regular;
+    return;
+  }
+
+  if (0 != S_ISFIFO(stat.st_mode)) {
+    type = myls::file_types::fifo;
+    return;
+  }
+
+  if (0 != S_ISLNK(stat.st_mode)) {
+    type = myls::file_types::symbolic_link;
+    return;
+  }
+
+  if (0 != S_ISSOCK(stat.st_mode)) {
+    type = myls::file_types::socket;
+    return;
+  }
+}
+
+
+char myls::file_type::operator()(void) {
+  switch (type) {
+    case myls::file_types::directory:
+      return 'd';
+
+    case myls::file_types::character:
+      return 'c';
+
+    case myls::file_types::block:
+      return 'b';
+
+    case myls::file_types::regular:
+      return '-';
+
+    case myls::file_types::fifo:
+      return 'p';
+
+    case myls::file_types::symbolic_link:
+      return 'l';
+
+    case myls::file_types::socket:
+      return 's';
+  }
+
+  return '\0';
+}
---
+++ myls/file_type.hxx
@@ -0,0 +1,45 @@
+#ifndef FILE_TYPE_HXX
+#define FILE_TYPE_HXX
+
+#include <cstdint>
+#include <string>
+#include <sys/stat.h>
+#include <unordered_map>
+
+namespace myls {
+  class file_type;
+
+  enum class file_types : std::uint8_t {
+    directory = 0b0000001u,
+    character = 0b0000010u,
+    block = 0b0000100u,
+    regular = 0b0001000u,
+    fifo = 0b0010000u,
+    symbolic_link = 0b0100000u,
+    socket = 0b1000000u
+  };
+}
+
+class myls::file_type {
+  file_types type: 7u;
+
+  using sys_stat = struct stat;
+  sys_stat stat{};
+
+  void fill_type(void);
+
+  template<typename Output>
+  friend Output& operator<<(Output& out, myls::file_type type);
+
+ public:
+  file_type(const std::string& name);
+
+  char operator()(void);
+};
+
+template<typename Output>
+Output& myls::operator<<(Output& out, myls::file_type type) {
+  return out << type();
+}
+
+#endif
