f19ff6ad70795e1a28dc18d3d57163f5eb9feefa
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-06-04 00:08:51 +0500

    Приведение главной функции в нормальное состояние

--- myls/myls.cxx
+++ myls/myls.cxx
@@ -1,7 +1,26 @@
-#include<iostream>
+#include <cstdlib>
+#include <iostream>
 #include "argparser.hxx"
+#include "former.hxx"
 
 int main(int nargs, char* args[]) {
-  myls::argparser parser{nargs, args};
-  std::cout << parser() << '\n';
+
+  try {
+    myls::argparser parser{nargs, args};
+    myls::former former{parser()};
+
+    auto info{former()};
+
+    for (auto file_info : former()) {
+      std::cout << file_info << '\n';
+    }
+
+  } catch(std::exception& ex) {
+    std::cerr << ex.what() << '\n';
+    return EXIT_FAILURE;
+
+  } catch(std::string& ex) {
+    std::cerr << ex << '\n';
+    return EXIT_FAILURE;
+  }
 }
