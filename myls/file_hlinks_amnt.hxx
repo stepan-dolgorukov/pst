9abaf80fcff5fb440a3eadf09834f5b0657139cb
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-06-03 14:08:08 +0500

    Класс для подсчёта hard links на файл

---
+++ myls/file_hlinks_amnt.hxx
@@ -0,0 +1,30 @@
+#ifndef FILE_HLINKS_AMNT_HXX
+#define FILE_HLINKS_AMNT_HXX
+
+#include <cstddef>
+#include <string>
+#include <sys/stat.h>
+
+namespace myls {
+  class file_hlinks_amnt;
+}
+
+class myls::file_hlinks_amnt {
+  std::size_t amount{0u};
+
+  using sys_stat = struct stat;
+  sys_stat stat{};
+
+  void fill_amount(void);
+
+  template<typename Output>
+  friend Output& operator<<(Output& out, myls::file_hlinks_amnt& fha) {
+    return out << fha();
+  };
+
+ public:
+  file_hlinks_amnt(const std::string& name);
+  std::size_t operator()(void);
+};
+
+#endif
