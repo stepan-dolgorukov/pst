3a49f13d9e18e06d4801eafddfd5bddea5c54fde
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-06-04 10:04:30 +0500

    Оператор std::string

--- myls/file_modif_time.cxx
+++ myls/file_modif_time.cxx
@@ -12,3 +12,12 @@ void myls::file_modif_time::fill_time(void) {
 auto myls::file_modif_time::operator()(void) -> decltype(time) {
   return time;
 }
+
+myls::file_modif_time::operator std::string() {
+  auto time{operator()()};
+  char output_mod_time[64u]{'\0'};
+
+  std::strftime(std::data(output_mod_time), std::size(output_mod_time),
+                "%b %e %H:%M", std::localtime(&time));
+  return output_mod_time;
+}
